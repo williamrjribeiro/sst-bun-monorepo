ff40cc11b21647d6d93cbafb16a043b4e1914703
williamwilliamrjribeiro <william.ribeiro@gmail.com>
2025-11-14 12:50:34 +0000

    Improves error handling in API Lambda Function

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,21 +1,33 @@
 import { Example } from "@sst-bun-monorepo/core/example";
-// import { Resource } from "sst";
+import { Resource } from "sst";
 
 export default {
 	async fetch(request: Request): Promise<Response> {
-		console.log(
-			"[api-fetch] x-amzn-function-arn:",
-			request.headers.get("x-amzn-function-arn"),
-		);
+		try {
+			console.log(
+				"[api-fetch] x-amzn-function-arn:",
+				request.headers.get("x-amzn-function-arn"),
+			);
 
-		// TODO: Figure out a way to make SST Resources available to the Lambda function.
-		const message = `${Example.hello()} Work around to access SST Resources: ${process.env["BUCKET_NAME"]}.`;
+			const message = `${Example.hello()} Work around to access SST Resources: ${Resource.MyBucket.name}.`;
 
-		return new Response(message, {
-			status: 200,
-			headers: {
-				"Content-Type": "text/plain",
-			},
-		});
+			return new Response(message, {
+				status: 200,
+				headers: {
+					"Content-Type": "text/plain",
+				},
+			});
+		} catch (error) {
+			const errorMessage =
+				error instanceof Error ? error.message : String(error);
+			console.error(`[api-fetch]`, { errorMessage, error });
+
+			return new Response(errorMessage, {
+				status: 500,
+				headers: {
+					"Content-Type": "text/plain",
+				},
+			});
+		}
 	},
 };
